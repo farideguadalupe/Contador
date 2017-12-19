55c5cbdc6a76bbdc3b120ef7a4a38b8782ff2476
faride guadalupe te cima <13470192@itcampeche.edu.mx>
2017-12-19 10:19:44 -0600

    ContadorApp:se agrego el manejo de eventos ala interfaz de usuario grafica

--- app/src/main/java/com/faridecima/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/faridecima/android/contadorapp/MainActivity.java
@@ -6,35 +6,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    Button mBotoncontar;
-    Button mBotonreiniciar;
-    EditText mcampoConteo;
-    int CONTADOR = 0;
+    private Button mBotoncontar;
+    private Button mBotonreiniciar;
+    private EditText mcampoConteo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CONTADOR ++;
-        mcampoConteo = (EditText) findViewById(R.id.etiqueta_conteo);
 
+        mcampoConteo = (EditText) findViewById(R.id.etiqueta_conteo);
+        mBotonreiniciar = (Button) findViewById(R.id.boton_reiniciar);
         mBotoncontar = (Button) findViewById(R.id.boton_contar);
+
         mBotoncontar.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                CONTADOR++;
-                mcampoConteo.setText(+CONTADOR);
+               String a = mcampoConteo.getText().toString();
+                int b = Integer.parseInt(a);
+                int c= b+1;
+                a=String.valueOf(c);
+                mcampoConteo.setText(a);
+
             }
         });
-        mBotonreiniciar = (Button) findViewById(R.id.boton_reiniciar);
-        mBotoncontar.setOnClickListener(new View.OnClickListener (){
-                      public void onClick(View v) {
-                              CONTADOR = 0;
-            mcampoConteo.setText(+CONTADOR);
+
+        mBotonreiniciar.setOnClickListener(new View.OnClickListener (){
+                      @Override
+                                               public void onClick(View v) {
+            mcampoConteo.setText("0");
         }
             }
         );
     }
-}
+
+    }
